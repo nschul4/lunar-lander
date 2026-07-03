9d7bc9485635d0c5183a4fc7e93e64623da0d372
Neal Schultz <nschul4@github>
2026-07-03 01:26:01 +0100

    remove unused code

--- src/common/createPolygon.ts
+++ src/common/createPolygon.ts
@@ -23,20 +23,3 @@ export function createPolygon(
     gameObject.setPosition(x, y);
     return gameObject;
 }
-
-export function createPolygonEx(
-    scene: Phaser.Scene,
-    points: string,
-    x: number,
-    y: number,
-    fillColor: number,
-    isStatic: boolean,
-    config: any
-): Phaser.GameObjects.GameObject {
-    var polygon = scene.add.polygon(0, 0, points, fillColor);
-    var gameObject: any = scene.matter.add.gameObject(polygon, config);
-    
-    // Set absolute position directly to match createPolygon
-    gameObject.setPosition(x, y);
-    return gameObject;
-}
