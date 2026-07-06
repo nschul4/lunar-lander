f65ab3ca0bf5aab60946871454563b66caf9f026
Neal Schultz <nschul4@github>
2026-07-06 22:41:08 +0100

    0706_1441

--- src/games/lander/game.ts
+++ src/games/lander/game.ts
@@ -40,7 +40,7 @@ const config = {
     default: 'matter',
     matter: {
       debug: true,
-      autoUpdate: false,
+      autoUpdate: true,
       fixedStep: true,
       debugShowInternalEdges: true,
       debugShowConvexHulls: true
