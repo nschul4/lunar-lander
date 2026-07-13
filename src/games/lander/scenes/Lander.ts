862e12a55a9409d79b120f478443e7b8d5695f7f
Neal Schultz <nschul4@github>
2026-07-13 16:13:29 +0100

    refactor(lander): remove  hacks and safely manipulate Matter body physics via official Phaser API

--- src/games/lander/scenes/Lander.ts
+++ src/games/lander/scenes/Lander.ts
@@ -3,9 +3,7 @@ import { ControllerScene } from "./controllerScene";
 
 export class Lander extends Phaser.GameObjects.Polygon {
   public thrust: Phaser.GameObjects.Polygon;
-  
-  // Expose the dynamically added matter body for internal TypeScript checks
-  public body!: MatterJS.BodyType; 
+  public body!: MatterJS.BodyType;
 
   private static readonly THRUST_FORCE = 0.00002;
   private static readonly ROTATION_SPEED = 0.01;
@@ -13,23 +11,29 @@ export class Lander extends Phaser.GameObjects.Polygon {
   constructor(scene: Phaser.Scene) {
     const xLeft = 0, xMiddle = 20, xRight = 40;
     const yTop = 0, yMiddle = 30, yBottom = 40;
-
     const landerPoints = `${xLeft} ${yBottom} ${xMiddle} ${yMiddle} ${xRight} ${yBottom} ${xMiddle} ${yTop}`;
 
     super(scene, 0, 0, landerPoints, 0x999999);
     scene.add.existing(this);
-
+    
     scene.matter.add.gameObject(this, {
       shape: { type: 'fromVerts', verts: landerPoints, flagInternal: true }
     });
-
+    
     this.angle = -90;
     this.setPosition(100, 200);
 
-    const physicsSelf = this as any;
-    physicsSelf.setVelocityX(1.5);
-    physicsSelf.setFrictionAir(0);
-    physicsSelf.setBounce(0, 0);
+    if (this.body) {
+      scene.matter.body.setVelocity(this.body, { x: 1.5, y: 0 });
+    }
+    
+    const matterGameObject = this as unknown as Phaser.Physics.Matter.Components.Velocity & Phaser.Physics.Matter.Components.Bounce;
+    if (typeof matterGameObject.setFrictionAir === 'function') {
+      matterGameObject.setFrictionAir(0);
+    }
+    if (typeof matterGameObject.setBounce === 'function') {
+      matterGameObject.setBounce(0, 0);
+    }
     
     this.setName("lander");
 
@@ -37,7 +41,6 @@ export class Lander extends Phaser.GameObjects.Polygon {
     const yTop2 = yMiddle + mysteryShifter;
     const yMiddle2 = yBottom + mysteryShifter;
     const yBottom2 = yBottom + mysteryShifter + 10;
-
     const thrustFlamePoints = `${xLeft} ${yMiddle2} ${xMiddle} ${yBottom2} ${xRight} ${yMiddle2} ${xMiddle} ${yTop2}`;
 
     this.thrust = scene.add.polygon(this.x, this.y, thrustFlamePoints, 0xffffff);
@@ -45,7 +48,6 @@ export class Lander extends Phaser.GameObjects.Polygon {
     this.thrust.visible = false;
   }
 
-  // ENCAPSULATION: Getters for physics properties
   public getVelocityX(): number {
     return this.body ? this.body.velocity.x : 0;
   }
@@ -55,15 +57,11 @@ export class Lander extends Phaser.GameObjects.Polygon {
   }
 
   public stop(): void {
-    const physicsSelf = this as any;
-    if (typeof physicsSelf.setVelocity === 'function') {
-      physicsSelf.setVelocity(0, 0);
+    if (this.body && this.scene && this.scene.matter) {
+      this.scene.matter.body.setVelocity(this.body, { x: 0, y: 0 });
+      this.scene.matter.body.setAngularVelocity(this.body, 0);
     }
-    if (typeof physicsSelf.setAngularVelocity === 'function') {
-      physicsSelf.setAngularVelocity(0);
-    }
-    // ENCAPSULATION: Reset angle internally
-    this.angle = 0; 
+    this.angle = 0;
   }
 
   public update(controllerScene: ControllerScene): void {
@@ -74,22 +72,24 @@ export class Lander extends Phaser.GameObjects.Polygon {
       const forceX = Lander.THRUST_FORCE * Math.sin(radians);
       const forceY = -Lander.THRUST_FORCE * Math.cos(radians);
       
-      (this.scene.matter as any).body.applyForce(this.body, this.body.position, {
-        x: forceX,
-        y: forceY
-      });
+      if (this.body) {
+        (this.scene.matter as any).body.applyForce(this.body, this.body.position, {
+          x: forceX,
+          y: forceY
+        });
+      }
     } else {
       this.thrust.visible = false;
     }
 
-    const physicsSelf = this as any;
-    physicsSelf.setAngularVelocity(0);
-    
-    if (controllerScene.rotatingLeft === true) {
-      physicsSelf.setAngularVelocity(-Lander.ROTATION_SPEED);
-    }
-    if (controllerScene.rotatingRight === true) {
-      physicsSelf.setAngularVelocity(Lander.ROTATION_SPEED);
+    if (this.body && this.scene && this.scene.matter) {
+      let angularVelocity = 0;
+      if (controllerScene.rotatingLeft === true) {
+        angularVelocity = -Lander.ROTATION_SPEED;
+      } else if (controllerScene.rotatingRight === true) {
+        angularVelocity = Lander.ROTATION_SPEED;
+      }
+      this.scene.matter.body.setAngularVelocity(this.body, angularVelocity);
     }
 
     this.thrust.x = this.x;
