261dd33b0e766c795dd417baedf4ee587ffd0145
Neal Schultz <nschul4@github>
2026-07-17 20:45:04 +0100

    refactor: move Logger to src root and update config imports

--- src/configs/1.ts
+++ src/configs/1.ts
@@ -4,7 +4,7 @@ import { GameSceneBoot } from "../scenes/gameSceneBoot";
 import { GameSceneOverview as GameSceneLevelOverview } from "../scenes/gameSceneLevelOverview";
 import { GameSceneOverlay } from "../scenes/gameSceneOverlay";
 import { GameSceneController } from "../scenes/gameSceneController";
-import { Logger } from "../common/classes/logger";
+import { Logger } from "../logger";
 import { g_version } from "../version";
 
 Logger.log("top of lander test/game.ts");
