eb073c60b54c888899801092398bfbc7801f60a2
Neal Schultz <nschul4@github>
2026-08-11 19:05:03 +0100

    feat: support URL level parameter in test subapp and update portal links

--- src/scenes/gameSceneInstrumented.ts
+++ src/scenes/gameSceneInstrumented.ts
@@ -1,9 +1,20 @@
 import { GameScene } from "./gameScene";
 import { drawMeasurementGrid } from "../gridOverlay";
+import { getLevelFromUrl } from "../levelUtil";
+import { LevelBlueprint } from "../levelBlueprints";
 
 export class GameSceneInstrumented extends GameScene {
   constructor() {
     super();
+  }
+
+  init(data?: { levelIndex?: number; level?: LevelBlueprint }): void {
+    if (data?.level || data?.levelIndex !== undefined) {
+      super.init(data);
+    } else {
+      const { level } = getLevelFromUrl();
+      super.init({ level });
+    }
   }
 
   create(): void {
